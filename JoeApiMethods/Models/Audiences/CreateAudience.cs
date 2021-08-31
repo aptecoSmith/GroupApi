83103e689d18ec4a761ca281f3770d66be79f246
AptecoJoe <89634494+AptecoJoe@users.noreply.github.com>
2021-08-31 17:24:32 +0100

    Add files via upload

---
+++ JoeApiMethods/Models/Audiences/CreateAudience.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIStuff.Models.Audiences
+{
+    public class CreateAudience
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class RecordSet
+        {
+            public string type { get; set; }
+            public string keyVariableName { get; set; }
+            public bool byReference { get; set; }
+            public string path { get; set; }
+            public bool transient { get; set; }
+            public string values { get; set; }
+            public int minOccurs { get; set; }
+        }
+
+        public class Logic
+        {
+            public string operation { get; set; }
+            public List<object> operands { get; set; }
+            public string tableName { get; set; }
+            public string name { get; set; }
+        }
+
+        public class AgeRule
+        {
+            public int rangeLow { get; set; }
+            public int rangeHigh { get; set; }
+            public string units { get; set; }
+            public string relativeTo { get; set; }
+            public string referenceType { get; set; }
+            public DateTime referenceDate { get; set; }
+        }
+
+        public class DateRule
+        {
+            public string patternFrequency { get; set; }
+            public int patternInterval { get; set; }
+            public string patternType { get; set; }
+            public List<string> patternDaysOfWeek { get; set; }
+            public int patternDayOfMonth { get; set; }
+            public int patternMonthOfYear { get; set; }
+            public string patternOccurrenceOfDayInMonth { get; set; }
+            public string startRangeLimit { get; set; }
+            public DateTime rangeStartDate { get; set; }
+            public string startRangeRelative { get; set; }
+            public string startRangeOffsetDirection { get; set; }
+            public int startRangeOffset { get; set; }
+            public string startRangeOffsetUnits { get; set; }
+            public string endRangeLimit { get; set; }
+            public DateTime rangeEndDate { get; set; }
+            public string endRangeRelative { get; set; }
+            public string endRangeOffsetDirection { get; set; }
+            public int endRangeOffset { get; set; }
+            public string endRangeOffsetUnits { get; set; }
+            public int rangeMaxOccurrences { get; set; }
+        }
+
+        public class ListRule
+        {
+            public string bandingType { get; set; }
+            public string list { get; set; }
+            public string variableName { get; set; }
+        }
+
+        public class TimeRule
+        {
+            public string rangeLow { get; set; }
+            public string rangeHigh { get; set; }
+        }
+
+        public class ValueRule
+        {
+            public AgeRule ageRule { get; set; }
+            public DateRule dateRule { get; set; }
+            public ListRule listRule { get; set; }
+            public TimeRule timeRule { get; set; }
+            public string predefinedRule { get; set; }
+            public string name { get; set; }
+        }
+
+        public class ExpressionRule
+        {
+            public string tableName { get; set; }
+            public List<object> queries { get; set; }
+            public string desc { get; set; }
+            public string displayText { get; set; }
+            public string serverText { get; set; }
+            public List<string> queryDescriptions { get; set; }
+            public string outputType { get; set; }
+            public int stringSize { get; set; }
+        }
+
+        public class Criteria
+        {
+            public string variableName { get; set; }
+            public string path { get; set; }
+            public bool include { get; set; }
+            public string logic { get; set; }
+            public bool ignoreCase { get; set; }
+            public string textMatchType { get; set; }
+            public List<ValueRule> valueRules { get; set; }
+            public ExpressionRule expressionRule { get; set; }
+            public string todayAt { get; set; }
+            public string tableName { get; set; }
+            public string name { get; set; }
+        }
+
+        public class SubSelection
+        {
+            public bool byReference { get; set; }
+            public string path { get; set; }
+        }
+
+        public class AudienceReference
+        {
+            public int audienceId { get; set; }
+            public string version { get; set; }
+            public bool include { get; set; }
+            public string tableName { get; set; }
+        }
+
+        public class FileReference
+        {
+            public string type { get; set; }
+            public string keyVariable { get; set; }
+            public string path { get; set; }
+            public int records { get; set; }
+            public bool include { get; set; }
+            public string tableName { get; set; }
+        }
+
+        public class Clause
+        {
+            public Logic logic { get; set; }
+            public Criteria criteria { get; set; }
+            public SubSelection subSelection { get; set; }
+            public AudienceReference audienceReference { get; set; }
+            public FileReference fileReference { get; set; }
+        }
+
+        public class Rule
+        {
+            public Clause clause { get; set; }
+        }
+
+        public class Frequency
+        {
+            public string values { get; set; }
+        }
+
+        public class Recency
+        {
+            public string variableName { get; set; }
+            public string sequence { get; set; }
+            public string direction { get; set; }
+            public int value { get; set; }
+            public bool distinct { get; set; }
+        }
+
+        public class Value
+        {
+            public string variableName { get; set; }
+            public string action { get; set; }
+            public string values { get; set; }
+        }
+
+        public class Rfv
+        {
+            public Frequency frequency { get; set; }
+            public Recency recency { get; set; }
+            public Value value { get; set; }
+            public string groupingTable { get; set; }
+            public string transactionalTable { get; set; }
+        }
+
+        public class NPer
+        {
+            public Recency recency { get; set; }
+            public string groupingTableName { get; set; }
+            public string transactionalTableName { get; set; }
+        }
+
+        public class OrderExpression
+        {
+            public string tableName { get; set; }
+            public List<object> queries { get; set; }
+            public string desc { get; set; }
+            public string displayText { get; set; }
+            public string serverText { get; set; }
+            public List<string> queryDescriptions { get; set; }
+            public string outputType { get; set; }
+            public int stringSize { get; set; }
+        }
+
+        public class TopN
+        {
+            public string variableName { get; set; }
+            public OrderExpression orderExpression { get; set; }
+            public string expression { get; set; }
+            public string direction { get; set; }
+            public int value { get; set; }
+            public int percent { get; set; }
+            public int minValue { get; set; }
+            public int maxValue { get; set; }
+            public string sequence { get; set; }
+            public string groupingVariableName { get; set; }
+            public string groupingSequenceVariableName { get; set; }
+            public bool groupingAscending { get; set; }
+            public string groupingSequence { get; set; }
+            public int groupMax { get; set; }
+        }
+
+        public class Fraction
+        {
+            public int numerator { get; set; }
+            public int denominator { get; set; }
+        }
+
+        public class Limits
+        {
+            public string sampling { get; set; }
+            public bool stopAtLimit { get; set; }
+            public int total { get; set; }
+            public string type { get; set; }
+            public int startAt { get; set; }
+            public int percent { get; set; }
+            public Fraction fraction { get; set; }
+        }
+
+        public class Selection
+        {
+            public bool ancestorCounts { get; set; }
+            public RecordSet recordSet { get; set; }
+            public Rule rule { get; set; }
+            public Rfv rfv { get; set; }
+            public NPer nPer { get; set; }
+            public TopN topN { get; set; }
+            public Limits limits { get; set; }
+            public string tableName { get; set; }
+            public string name { get; set; }
+        }
+
+        public class ExcludeQuery
+        {
+            public Selection selection { get; set; }
+            public string todayAt { get; set; }
+        }
+
+        public class IncludeQuery
+        {
+            public Selection selection { get; set; }
+            public string todayAt { get; set; }
+        }
+
+        public class BodyQuery
+        {
+            public Selection selection { get; set; }
+            public string todayAt { get; set; }
+        }
+
+        public class SelectionModifiers
+        {
+            public Limits limits { get; set; }
+            public TopN topN { get; set; }
+            public NPer nPer { get; set; }
+            public Rfv rfv { get; set; }
+        }
+
+        public class Root
+        {
+            public string title { get; set; }
+            public string description { get; set; }
+            public ExcludeQuery excludeQuery { get; set; }
+            public IncludeQuery includeQuery { get; set; }
+            public BodyQuery bodyQuery { get; set; }
+            public SelectionModifiers selectionModifiers { get; set; }
+            public string briefText { get; set; }
+            public string campaignId { get; set; }
+            public string systemName { get; set; }
+        }
+
+
+    }
+}
---
+++ JoeApiMethods/ObritAPIMethods/Audiences.cs
@@ -0,0 +1,214 @@
+﻿using APIStuff.Models.Audiences;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace APIStuff.ObritAPIMethods
+{
+    public class Audiences
+    {
+        readonly string _baseURL;
+        readonly string _dateView = "test";
+        public string _accessToken;
+        public string _userName = "joe.barradell@apteco.com";
+        public string _systemName = "Test";
+        private readonly OrbitAPIMethods _orbitAPIMethods;
+
+        public Audiences(string baseURL)
+        {
+            _baseURL = baseURL;
+            _orbitAPIMethods = new OrbitAPIMethods(baseURL);
+        }
+
+        public void GetAudiences(string localAccessToken, int count = 0, int? id = null)
+        {
+            //access token must be valid
+            if (localAccessToken == null)
+            {
+                Console.WriteLine("No valid access token");
+                return;
+            }
+
+            //set the url using the class variables and the variable passed from Program.cs
+            var allAudienceURL = string.Join("/", _baseURL, _dateView, "/Audiences");
+            var oneAudienceURL = string.Join("/", _baseURL, _dateView, "/Audiences/", id);
+
+            if (id == null)
+            {
+                IRestClient client = new RestClient(allAudienceURL);
+                //set up the rest API client and request 
+                IRestRequest request = new RestRequest("", Method.GET);
+
+                //add parameters to the request
+                request.AddHeader("Authorization", "Bearer " + localAccessToken);
+
+                //Do we want to change the default count?
+                if (count > 0)
+                {
+                    request.AddParameter("count", count);
+                }
+
+                IRestResponse <ListOfAudiences.Root> response = client.Execute<ListOfAudiences.Root>(request);
+
+                //execute the request through restAPI
+
+                var audienceList = response.Data.list;
+
+                //var text = JsonConvert.DeserializeObject<List<Audiences.Root>>(response);
+                var json = JsonConvert.SerializeObject(response.Content, Formatting.Indented);
+                File.WriteAllText(@"D:\Audiences.json", json);
+
+                List<string> resultsList = new List<string>();
+
+                foreach (var audience in audienceList)
+                {
+                    var title = audience.title;
+                    resultsList.Add(title);
+                }
+
+                string responseResult;
+                if (response.IsSuccessful)
+                {
+                    foreach (var item in resultsList)
+                    {
+                        Console.Write("{0}\t", item.ToString());
+                    }
+                }
+                else
+                {
+                    responseResult = response.StatusDescription;
+                    Console.WriteLine(responseResult);
+                }
+            }
+            else
+            {
+                IRestClient client = new RestClient(oneAudienceURL);
+                //set up the rest API client and request 
+                IRestRequest request = new RestRequest("", Method.GET);
+
+                //add parameters to the request
+                request.AddHeader("Authorization", "Bearer " + localAccessToken);
+                IRestResponse<SingleAudience.Root> response = client.Execute<SingleAudience.Root>(request);
+
+                var text = JsonConvert.DeserializeObject(response.Content.ToString());
+                var json = JsonConvert.SerializeObject(text, Formatting.Indented);
+                File.WriteAllText(@"D:\Audience.json", json);
+
+                string responseResult;
+                if (response.IsSuccessful)
+                {
+                    responseResult = response.Data.title;
+                    Console.WriteLine(responseResult);
+                }
+                else
+                {
+                    responseResult = response.StatusDescription;
+                    Console.WriteLine(responseResult);
+                }
+            }
+        }
+
+        public int GetNamedAudienceId(string localAccessToken, string searchName)
+        {
+            int audienceId = 0;
+            //access token must be valid
+            if (localAccessToken == null)
+            {
+                Console.WriteLine("No valid access token");
+                return 0;
+            }
+
+            //set the url using the class variables and the variable passed from Program.cs
+            var allAudienceURL = String.Join("/", _baseURL, _dateView, "/Audiences");
+
+            IRestClient client = new RestClient(allAudienceURL);
+            //set up the rest API client and request 
+            IRestRequest request = new RestRequest("", Method.GET);
+
+            //add parameters to the request
+            request.AddHeader("Authorization", "Bearer " + localAccessToken);
+            request.AddParameter("count", "5000");
+
+            Console.WriteLine("Getting response");
+            //execute the request through restAPI
+            IRestResponse<ListOfAudiences.Root> response = client.Execute<ListOfAudiences.Root>(request);
+
+            Console.WriteLine("Response received");
+            var audienceList = response.Data.list;
+
+            List<string> resultsList = new List<string>();
+
+            Console.WriteLine("Linq processing");
+            var audienceDefinition = audienceList.FirstOrDefault(x => x.title == searchName);
+            audienceId = audienceDefinition.id;
+
+            //make json pretty
+            var json = JsonConvert.SerializeObject(audienceDefinition, Formatting.Indented);
+            //save to file
+            File.WriteAllText(@"D:\SingleAudience.json", json);
+
+            Console.WriteLine("For loop processing");
+            foreach (var audience in audienceList)
+            {
+                Console.WriteLine(audience.title);
+            }
+
+            return audienceId;
+        }
+
+        public CreateAudience.Root ReadAudienceJsonFile(string filePath)
+        {
+            var readFile = File.ReadAllText($@"{filePath}");
+            CreateAudience.Root objectJson = JsonConvert.DeserializeObject<CreateAudience.Root>(readFile);
+
+            return objectJson;
+        }
+
+        public CreateAudience.Root ChangeAudienceJsonObjectTitle(CreateAudience.Root audience, string newTitle)
+        {
+            audience.title = newTitle;
+
+            return audience;
+        }
+
+        public string SerializeEditiedAudience(CreateAudience.Root editedAudience)
+        {
+            var json = JsonConvert.SerializeObject(editedAudience, Formatting.Indented);
+
+            return json;
+        }
+
+        public void CreateAudience(string localAccessToken, string json)
+        {
+            //access token must be valid
+            if (localAccessToken == null)
+            {
+                Console.WriteLine("No valid access token");
+            }
+
+            var createAuidenceURL = String.Join("/", _baseURL, _dateView, "/Audiences");
+            IRestClient client = new RestClient(createAuidenceURL);
+            //set up the rest API client and request 
+            IRestRequest request = new RestRequest("", Method.POST);
+
+            //add parameters to the request
+            request.AddHeader("Authorization", "Bearer " + localAccessToken);
+            //ensure correct media type
+            request.AddHeader("Accept", "application/json");
+            //add the json to body
+            request.AddJsonBody(json);
+
+            IRestResponse<CreateAudience.Root> response = client.Execute<CreateAudience.Root>(request);
+
+            //get the location of created audience from the response header
+            var location = response.Headers[2];
+
+            Console.WriteLine(location);
+        }
+
+    }
+}
