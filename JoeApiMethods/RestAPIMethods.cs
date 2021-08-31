48df6da7ac1f82d7ba8cd75bc25bb209fc1eb3bb
AptecoJoe <89634494+AptecoJoe@users.noreply.github.com>
2021-08-31 17:18:16 +0100

    Add files via upload

---
+++ JoeApiMethods/RestAPIMethods.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIStuff
+{
+    public class RestAPIMethods
+    {
+        public void GetAuthToken()
+        {
+
+        }
+    }
+}
